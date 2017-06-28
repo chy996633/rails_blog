d334692eadd4e8424630c29a07b354a0bce70733
Samuel Chen <samuel.chen@backstopsolutions.com>
2017-06-28 04:26:58 -0500

    resolved error in config.rb and added CUR operations

--- another_blog/app/controllers/articles_controller.rb
+++ another_blog/app/controllers/articles_controller.rb
@@ -4,18 +4,37 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def index
+    @articles = Article.all
+  end
+
   def new
   end
 
-  def create
-    # @article = Article.new(article_params)
-   
-    @article = Article.new(params.require(:article).permit(:title, :text))
-
-    @article.save
-    redirect_to @article
+  def edit
+    @article = Article.find(params[:id])
   end
-  
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
   private
   def article_params
     params.require(:article).permit(:title, :text)
