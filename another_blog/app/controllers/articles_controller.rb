c0645f93e5b944cc7c87a20ae18967c8e35cb410
Samuel Chen <samuel.chen@backstopsolutions.com>
2017-06-28 21:06:37 -0500

    use partial to reuse code

--- another_blog/app/controllers/articles_controller.rb
+++ another_blog/app/controllers/articles_controller.rb
@@ -1,14 +1,15 @@
 class ArticlesController < ApplicationController
   
-  def show
-    @article = Article.find(params[:id])
-  end
-
   def index
     @articles = Article.all
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
+    @article = Article.new
   end
 
   def edit
